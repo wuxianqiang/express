9fd672c8597a2484410f2502f3f95d5d7ecee5cd
wuxianqiang <2631640352@qq.com>
2019-06-22 16:55:26 +0800

    正则映射路由

--- express/index.js
+++ express/index.js
@@ -1,27 +1,77 @@
 const http = require('http')
 const url = require('url')
+const pathRegexp = require('./pathRegexp')
 
 function express() {
   const app = {}
-  const routes = [];
-  app.use = function (path, action) {
-    routes.push([path, action])
-  }
+  const routes = { 'all': [] };
 
-  function handle(req, res) {
-    let pathname = url.parse(req.url).pathname;
-    for (let i = 0; i < routes.length; i++) {
-      var route = routes[i];
-      if (pathname === route[0]) {
+  app.use = function (path, action) {
+    const keys = []
+    const regexp = pathRegexp(path, keys,{end:true})
+    routes.all.push([
+      { regexp, keys },
+      action
+    ]);
+  };
+
+  ['get', 'put', 'delete', 'post'].forEach(function (method) {
+    routes[method] = [];
+    app[method] = function (path, action) {
+      const keys = []
+      const regexp = pathRegexp(path, keys, {end:true})
+      routes[method].push([
+        { regexp, keys },
+        action
+      ]);
+    };
+  });
+
+  const match = function (pathname, routes, req, res) {
+    for (var i = 0; i < routes.length; i++) {
+      let route = routes[i];
+      let reg = route[0].regexp;
+      let keys = route[0].keys;
+      let matched = reg.exec(pathname);
+      if (matched) {
+        let params = {};
+        for (let i = 0, l = keys.length; i < l; i++) {
+          let value = matched[i + 1];
+          if (value) {
+            params[keys[i]] = value;
+          }
+        }
+        req.params = params;
         let action = route[1];
         action(req, res);
+        return true;
+      }
+    }
+    return false;
+  };
+
+
+  function handle(req, res) {
+    let {pathname, query} = url.parse(req.url, true);
+    req.query = query
+    let method = req.method.toLowerCase();
+    if (routes.hasOwnProperty(method)) {
+      if (match(pathname, routes[method], req, res)) {
+        return;
+      } else {
+        if (match(pathname, routes.all, req, res)) {
+          return;
+        }
+      }
+    } else {
+      if (match(pathname, routes.all, req, res)) {
         return;
       }
     }
     handle404(req, res);
   }
 
-  function handle404(req, res) {
+  function handle404 (req, res) {
     res.end('404')
   }
 
@@ -31,7 +81,6 @@ function express() {
     })
     server.listen(...args)
   }
-  
   return app
 }
 
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('./express')
 const app = express()
 
-app.use('/home', (req, res) => {
+app.use('/home/:id', (req, res) => {
+  console.log(req.params)
   res.end('home')
+})
+
+app.get('/user', (req, res) => {
+  console.log(req.query)
+  res.end('user')
 })
 
 app.listen(8080, () => {
